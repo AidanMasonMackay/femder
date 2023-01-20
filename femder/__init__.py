00dd0d57c550780c6b8478cd92bb9ba3cf317fe5
AidanMasonMackay <aidan@engendertechnologies.com>
2023-01-20 15:03:53 -0500

    remove tmm import (throws error)

--- femder/__init__.py
+++ femder/__init__.py
@@ -16,7 +16,7 @@ from femder.GeometryGenerate import GeometryGenerator
 from femder.optimization_helpers import r_s_for_room, r_s_positions,r_s_from_grid,fitness_metric
 from femder.utils import IR, SBIR
 from femder.BEM_3D import BEM3D
-from tmm.tmm import TMM
+#from tmm.tmm import TMM
 from femder import plot_tools
 from femder import fem_numerical
 
